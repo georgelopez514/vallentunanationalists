8b92dabb9dcb79cff3678ec93acdb5c7994864c1
zion <a25zioma@student.his.se>
2026-04-10 15:17:45 +0200

    i can see the head

---
+++ Assets/StolenCOde/Direshon.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Direshon : MonoBehaviour
+{
+    enum MovmentDireshon{
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
---
+++ Assets/StolenCOde/InputReader.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class InputReader : MonoBehaviour
+{
+    public Direshon dir;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Direshon curentDirecshon(){
+        if(Input.GetAxis("Horizontal") == 1){
+            
+        }
+        else if(Input.GetAxis("Horizontal") == -1){
+
+        }
+
+    }
+}
