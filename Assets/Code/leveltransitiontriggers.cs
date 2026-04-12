85d5e2303cdfc5c835842bbdefde1ea01b3f34a0
zion <121035557+ZiondamanSE@users.noreply.github.com>
2026-04-12 10:21:31 +0200

    ye am kinda big and round with the music

--- Assets/Code/leveltransitiontriggers.cs
+++ Assets/Code/leveltransitiontriggers.cs
@@ -4,11 +4,23 @@ using UnityEngine.SceneManagement;
 public class LevelTransitionTrigger : MonoBehaviour
 {
     [SerializeField] private string levelToLoad;
-
+    public bool killthemusic;
+    public string musicObjectName = "djmusic";
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.CompareTag("Player")) return;
+
+        if (killthemusic)
+        {
+            GameObject music = GameObject.Find(musicObjectName);
+            if (music != null)
+            {
+                Destroy(music);
+            }
+        }
+        else {
+            DontDestroyOnLoad(GameObject.Find(musicObjectName));
+        }
 
         Debug.Log($"[LevelTransitionTrigger] Loading: {levelToLoad}");
         SceneManager.LoadScene(levelToLoad);
---
+++ Assets/Code/musicomymusic.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class musicomymusic : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
